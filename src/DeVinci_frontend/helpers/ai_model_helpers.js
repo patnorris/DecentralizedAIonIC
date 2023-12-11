0f1c888aa46b61dcd6c663e7d28248f08f0612b3
patnorris <patrick@DESKTOP-TQ4V8LQ>
2023-12-11 02:15:47 +0100

    Add new models

--- src/DeVinci_frontend/helpers/ai_model_helpers.js
+++ src/DeVinci_frontend/helpers/ai_model_helpers.js
@@ -1,6 +1,6 @@
 const availableAiModels = [
   {
-    id: 'RedPajama-INCITE-Chat-3B-v1-q4f32_0',
+    id: 'RedPajama-INCITE-Chat-3B-v1-q4f32_1',
     name: 'Red Pajama',
     size: 'Medium',
     numberOfParameters: '3 billion',
@@ -8,16 +8,64 @@ const availableAiModels = [
     default: true
   },
   {
-    id: 'Llama-2-7b-chat-hf-q4f32_1',
-    name: 'Llama2',
+    id: 'Mistral-7B-Instruct-v0.1-q4f32_1',
+    name: 'Mistral',
     size: 'Large',
     numberOfParameters: '7 billion',
     performance: 'Very Good',
     default: false
   },
   {
-    id: 'Mistral-7B-OpenOrca-q4f32_1',
-    name: 'Mistral',
+    id: 'Llama-2-7b-chat-hf-q4f32_1',
+    name: 'Llama2 7b',
+    size: 'Large',
+    numberOfParameters: '7 billion',
+    performance: 'Very Good',
+    default: false
+  },
+  {
+    id: 'Llama-2-13b-chat-hf-q4f32_1',
+    name: 'Llama2 13b',
+    size: 'Very Large',
+    numberOfParameters: '13 billion',
+    performance: 'Super Good',
+    default: false
+  },
+  /* {
+    id: 'Llama-2-70b-chat-hf-q4f16_1',
+    name: 'Llama2 70b',
+    size: 'Gigantic',
+    numberOfParameters: '70 billion',
+    performance: 'Insane',
+    default: false
+  }, */
+  {
+    id: 'WizardCoder-15B-V1.0-q4f32_1',
+    name: 'WizardCoder',
+    size: 'Very Large',
+    numberOfParameters: '15 billion',
+    performance: 'Super Good',
+    default: false
+  },
+  {
+    id: 'WizardMath-7B-V1.0-q4f32_1',
+    name: 'WizardMath',
+    size: 'Large',
+    numberOfParameters: '7 billion',
+    performance: 'Very Good',
+    default: false
+  },
+  {
+    id: 'OpenHermes-2.5-Mistral-7B-q4f32_1',
+    name: 'OpenHermes 2.5',
+    size: 'Large',
+    numberOfParameters: '7 billion',
+    performance: 'Very Good',
+    default: false
+  },
+  {
+    id: 'NeuralHermes-2.5-Mistral-7B-q4f32_1',
+    name: 'NeuralHermes 2.5',
     size: 'Large',
     numberOfParameters: '7 billion',
     performance: 'Very Good',
