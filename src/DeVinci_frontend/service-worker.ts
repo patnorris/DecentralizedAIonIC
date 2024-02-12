309bc0263ed6cfdbbeaca42ade46c6604db5c944
patnorris <patrick@DESKTOP-TQ4V8LQ>
2024-02-12 19:05:22 +0100

    Load service worker from raw app url

--- src/DeVinci_frontend/service-worker.ts
+++ src/DeVinci_frontend/service-worker.ts
@@ -1,3 +1,5 @@
+console.log('Service Worker file START');
+
 import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching';
 import { clientsClaim } from 'workbox-core';
 
@@ -11,16 +13,57 @@ self.skipWaiting();
 clientsClaim();
 
 self.addEventListener('message', (event) => {
+    console.log('Service Worker file message');
     if (event.data && event.data.type === 'SKIP_WAITING')
       self.skipWaiting()
   });
 
-console.log('Service Worker file.');
-
-/* self.addEventListener('install', (event) => {
+self.addEventListener('install', (event) => {
     console.log('Service Worker installing.');
 });
 
 self.addEventListener('activate', (event) => {
     console.log('Service Worker activated.');
-}); */
+});
+
+console.log('Service Worker file END');
+
+/* 
+/// <reference lib="webworker" />
+declare const self: ServiceWorkerGlobalScope;
+
+const CACHE_NAME = 'v1-cache';
+const urlsToCache = [
+  // List URLs to cache here
+  '/',
+  '/index.html',
+  // Add other assets (CSS, JavaScript, images, etc.)
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event to cache all requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cache hit or fetch and cache
+        return response || fetch(event.request).then(fetchResponse => {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, fetchResponse.clone());
+            return fetchResponse;
+          });
+        });
+      })
+  );
+});
+*/
