5c209e4cf4ee8e3c14f45aa013358b211e1d14b2
patnorris <patrick@DESKTOP-TQ4V8LQ>
2024-03-04 13:01:32 +0100

    Clean code

--- src/DeVinci_frontend/main.ts
+++ src/DeVinci_frontend/main.ts
@@ -1,22 +1,5 @@
 import App from "./App.svelte"
 import "./app.css"
-/* import {useRegisterSW} from 'virtual:pwa-register/svelte';
-
-const {offlineReady, needRefresh, updateServiceWorker} = useRegisterSW({
-    async onRegistered(swr) {
-        console.log(`SW registered: `, swr);
-        console.log(`SW installing: `, swr.installing);
-        console.log(`SW navigationPreload: `, swr.navigationPreload);
-        console.log(`SW navigationPreload getState: `, await swr.navigationPreload.getState());
-    },
-    onRegisterError(error) {
-        console.log('SW registration error', error);
-    },
-    onOfflineReady() {
-        console.log('SW ready for offline')
-        //setTimeout(() => close(), 5000)
-    }
-}); */
 
 const app = new App({
   target: document.getElementById("root"),
