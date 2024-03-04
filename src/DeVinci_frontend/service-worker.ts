44f37fdbe1e44b2b31465f81ca76e322901fa4ab
patnorris <patrick@DESKTOP-TQ4V8LQ>
2024-03-04 14:04:27 +0100

    Enable cache first approach and full offline functionality

--- src/DeVinci_frontend/service-worker.ts
+++ src/DeVinci_frontend/service-worker.ts
@@ -1,7 +1,7 @@
-console.log('Service Worker file START');
-
 import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching';
 import { clientsClaim } from 'workbox-core';
+import { registerRoute } from 'workbox-routing';
+import { CacheFirst } from 'workbox-strategies';
 
 declare let self: ServiceWorkerGlobalScope;
 
@@ -16,8 +16,28 @@ const CACHE_NAME = 'devinci-cache';
 const urlsToCache = [
   // List URLs to cache here
   '/',
+  '/*',
+  '/**',
   '/index.html',
+  '/.well-known',
+  '/.well-known/ii-alternative-origins',
+  '/assets',
+  '/assets/**',
+  '/assets/index-61ac7e9e.js',
+  '/assets/index-81637ee6.js',
+  '/assets/index-ebbe5121.css',
+  '/assets/loading-44d9ce91.gif',
+  '/serviceWorker',
+  '/serviceWorker/**',
+  '/serviceWorker/service-worker.js',
   // Add other assets (CSS, JavaScript, images, etc.)
+  '.ic-assets.json',
+  'apple-touch-icon.png',
+  'favicon.ico',
+  'loading.gif',
+  'main.css',
+  'manifest.webmanifest',
+  'worker.ts',
 ];
 
 // Install event
@@ -36,33 +56,14 @@ self.addEventListener('activate', (event) => {
   console.log('Service Worker activated.');
 });
 
-// Fetch event to cache all requests
-self.addEventListener('fetch', event => {
-  console.log('Service Worker file fetch event ', event);
-  console.log('Service Worker file fetch event.request ', event.request);
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Return cache hit or fetch and cache
-        console.log('Service Worker file fetch response ', response);
-        return response || fetch(event.request).then(fetchResponse => {
-          console.log('Service Worker file fetch fetchResponse ', fetchResponse);
-          return caches.open(CACHE_NAME).then(cache => {
-            console.log('Service Worker file fetch cache ', cache);
-            cache.put(event.request, fetchResponse.clone());
-            return fetchResponse;
-          });
-        });
-      })
-  );
-});
-
-self.addEventListener('message', (event) => {
-  console.log('Service Worker file message');
-  if (event.data && event.data.type === 'SKIP_WAITING')
-    self.skipWaiting()
-});
-
-
-console.log('Service Worker file END');
+// Use a CacheFirst strategy for all requests
+registerRoute(
+  ({ request }) => true,
+  new CacheFirst({
+    cacheName: CACHE_NAME,
+    plugins: [
+      // Optionally, configure plugins, e.g., to limit cache entries or expiration
+    ],
+  })
+);
 
