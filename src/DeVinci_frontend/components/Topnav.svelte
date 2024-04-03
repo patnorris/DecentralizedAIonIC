e217c3ec2de3c278558d7579edbc29b2422d65e7
patnorris <patrick@DESKTOP-TQ4V8LQ>
2024-04-03 15:37:01 +0200

    Update tab title

--- src/DeVinci_frontend/components/Topnav.svelte
+++ src/DeVinci_frontend/components/Topnav.svelte
@@ -1,5 +1,5 @@
 <div class="devinci-topnav flex items-center justify-around flex-wrap p-3 text-l font-bold text-white">
-  <a href='#top'>DeVinci AI Assistant</a>
+  <a href='#top'>AI Assistant</a>
   <a href='/#/mychats'>My Chats</a>
   <a href='/#/about'>About</a>
 </div>
