7d9d594f5f7ed89f222d405a5ab9c4b6c6736d91
Nuno <n@lopezi.com>
2024-06-13 12:39:40 +0100

    center login modal

--- src/DeVinci_frontend/components/new/Login.svelte
+++ src/DeVinci_frontend/components/new/Login.svelte
@@ -52,7 +52,7 @@
 
 <!-- Main modal -->
 <div id="crypto-modal" tabindex="-1" aria-hidden="true" class="hidden bg-gray-900/[.36] overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%)] max-h-full">
-  <div class="relative p-4 w-full mx-auto flex items-center content-center max-w-md max-h-full h-full">
+  <div class="relative p-4 w-full mx-auto flex items-center content-center max-w-sm max-h-full h-full">
     <!-- Modal content -->
     <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
       <!-- Modal header -->
--- src/DeVinci_frontend/components/new/SelectModel.svelte
+++ src/DeVinci_frontend/components/new/SelectModel.svelte
@@ -2,7 +2,7 @@
 
 </script>
 
-  <ul class="grid w-full gap-0 md:grid-cols-2 px-4 mt-4">
+  <ul class="grid w-full gap-2 md:grid-cols-2 px-4 mt-4">
     <li>
       <input type="radio" id="TinyLlama" name="models" value="TinyLlama" class="hidden peer">
       <label for="TinyLlama" class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
