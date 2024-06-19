d63134480ad304fdeb48976ac30ec50bb840e908
Nuno <n@lopezi.com>
2024-06-19 14:24:15 +0100

    SelectModel into panel card

--- src/DeVinci_frontend/components/new/SelectModel.svelte
+++ src/DeVinci_frontend/components/new/SelectModel.svelte
@@ -1,106 +1,85 @@
 <script lang="ts">
+  import SelectModelOption from './SelectModelOption.svelte';
 
+  const modelOptions = [
+    {
+      id: "TinyLlama",
+      name: "models",
+      value: "TinyLlama",
+      title: "TinyLlama",
+      parameters: "1.1 billion parameters",
+      performance: "Alright performance",
+      size: "Small size"
+    },
+    {
+      id: "Gemma",
+      name: "models",
+      value: "Gemma",
+      title: "Gemma",
+      parameters: "2 billion parameters",
+      performance: "Good performance",
+      size: "Small size"
+    },
+    {
+      id: "RedPajama",
+      name: "models",
+      value: "RedPajama",
+      title: "Red Pajama",
+      parameters: "3 billion parameters",
+      performance: "Alright performance",
+      size: "Small size"
+    },
+    {
+      id: "Mistral",
+      name: "models",
+      value: "Mistral",
+      title: "Mistral",
+      parameters: "7 billion parameters",
+      performance: "Very good performance",
+      size: "Large size"
+    },
+    {
+      id: "Llama38B",
+      name: "models",
+      value: "Llama38B",
+      title: "Llama 3 8B",
+      parameters: "8 billion parameters",
+      performance: "Super good performance",
+      size: "Large size"
+    },
+    {
+      id: "Llama213B",
+      name: "models",
+      value: "Llama213B",
+      title: "Llama2 13B",
+      parameters: "13 billion parameters",
+      performance: "Super good performance",
+      size: "Large size"
+    },
+    {
+      id: "Llama370B",
+      name: "models",
+      value: "Llama370B",
+      title: "Llama3 70B",
+      parameters: "70 billion parameters",
+      performance: "Insane performance",
+      size: "Very large size"
+    }
+  ];
 </script>
 
   <ul class="grid w-full gap-2 md:grid-cols-2 px-4 mt-4">
-    <li>
-      <input type="radio" id="TinyLlama" name="models" value="TinyLlama" class="hidden peer">
-      <label for="TinyLlama" class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
-        <div class="block">
-          <div class="w-full text-lg font-semibold">TinyLlama</div>
-          <div class="w-full text-md font-normal">1.1 billion parameters</div>
-          <span class="bg-blue-50 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">Alright performance</span>
-          <span class="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-400 border border-gray-500">Small size</span>
-        </div>
-        <svg class="w-5 h-5 ms-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-        </svg>
-      </label>
-    </li>
-    <li>
-      <input type="radio" id="Gemma" name="models" value="Gemma" class="hidden peer" required />
-      <label for="Gemma" class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
-        <div class="block">
-          <div class="w-full text-lg font-semibold">Gemma</div>
-          <div class="w-full text-md font-normal">2 billion parameters</div>
-          <span class="bg-blue-50 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">Good performance</span>
-          <span class="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-400 border border-gray-500">Small size</span>
-        </div>
-        <svg class="w-5 h-5 ms-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-        </svg>
-      </label>
-    </li>
-    <li>
-      <input type="radio" id="RedPajama" name="models" value="RedPajama" class="hidden peer" required />
-      <label for="RedPajama" class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
-        <div class="block">
-          <div class="w-full text-lg font-semibold">Red Pajama</div>
-          <div class="w-full text-md font-normal">3 billion parameters</div>
-          <span class="bg-blue-50 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">Alright performance</span>
-          <span class="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-400 border border-gray-500">Small size</span>
-        </div>
-        <svg class="w-5 h-5 ms-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-        </svg>
-      </label>
-    </li>
-    <li>
-      <input type="radio" id="Mistral" name="models" value="Llama38B" class="hidden peer" required />
-      <label for="Mistral" class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
-        <div class="block">
-          <div class="w-full text-lg font-semibold">Mistral</div>
-          <div class="w-full text-md font-normal">7 billion parameters</div>
-          <span class="bg-blue-50 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">Very good performance</span>
-          <span class="bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-yellow-300 border border-yellow-300">Large size</span>
-        </div>
-        <svg class="w-5 h-5 ms-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-        </svg>
-      </label>
-    </li>
-    <li>
-      <input type="radio" id="Llama38B" name="models" value="Llama38B" class="hidden peer" required />
-      <label for="Llama38B" class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
-        <div class="block">
-          <div class="w-full text-lg font-semibold">Llama 3 8B</div>
-          <div class="w-full text-md font-normal">8 billion parameters</div>
-          <span class="bg-blue-50 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">Super good performance</span>
-          <span class="bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-yellow-300 border border-yellow-300">Large size</span>
-        </div>
-        <svg class="w-5 h-5 ms-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-        </svg>
-      </label>
-    </li>
-    <li>
-      <input type="radio" id="Llama213B" name="models" value="Llama213B" class="hidden peer" required />
-      <label for="Llama213B" class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
-        <div class="block">
-          <div class="w-full text-lg font-semibold">Llama2 13B</div>
-          <div class="w-full text-md font-normal">13 billion parameters</div>
-          <span class="bg-blue-50 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">Super good performance</span>
-          <span class="bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-yellow-300 border border-yellow-300">Large size</span>
-        </div>
-        <svg class="w-5 h-5 ms-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-        </svg>
-      </label>
-    </li>
-    <li>
-      <input type="radio" id="Llama370B" name="models" value="Llama370B" class="hidden peer" required />
-      <label for="Llama370B" class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
-        <div class="block">
-          <div class="w-full text-lg font-semibold">Llama3 70B</div>
-          <div class="w-full text-md font-normal">70 billion parameters</div>
-          <span class="bg-blue-50 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">Insane performance</span>
-          <span class="bg-purple-100 text-purple-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-purple-400 border border-purple-400">Very large size</span>
-        </div>
-        <svg class="w-5 h-5 ms-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-        </svg>
-      </label>
-    </li>
+    {#each modelOptions as option}
+      <SelectModelOption
+        id={option.id}
+        name={option.name}
+        value={option.value}
+        title={option.title}
+        parameters={option.parameters}
+        performance={option.performance}
+        size={option.size}
+      />
+    {/each}
   </ul>
 
 <style>
---
+++ src/DeVinci_frontend/components/new/SelectModelOption.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let id;
+  export let name;
+  export let value;
+  export let title;
+  export let parameters;
+  export let performance;
+  export let size;
+</script>
+
+<li>
+  <input type="radio" id={id} name={name} value={value} class="hidden peer" />
+  <label for={id} class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+    <div class="block">
+      <div class="w-full text-lg font-semibold">{title}</div>
+      <div class="w-full text-md font-normal">{parameters}</div>
+      <span class="bg-blue-50 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">{performance}</span>
+      <span class="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-400 border border-gray-500">{size}</span>
+    </div>
+    <svg class="w-5 h-5 ms-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+    </svg>
+  </label>
+</li>
