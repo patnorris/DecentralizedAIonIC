3b98eb973b7ab8bc17b655813f1db1d87b43af7a
Nuno <n@lopezi.com>
2024-06-19 17:44:14 +0100

    remove add question

--- src/DeVinci_frontend/components/new/Login.svelte
+++ src/DeVinci_frontend/components/new/Login.svelte
@@ -171,13 +171,6 @@
             </a>
           </li>
         </ul>
-        <div>
-          <a href="#" class="inline-flex items-center text-xs font-normal text-gray-500 hover:underline dark:text-gray-400">
-            <svg class="w-3 h-3 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7.529 7.988a2.502 2.502 0 0 1 5 .191A2.441 2.441 0 0 1 10 10.582V12m-.01 3.008H10M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
-            </svg>
-            Why do I need to connect with my wallet?</a>
-        </div>
       </div>
     </div>
   </div>
