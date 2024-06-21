8fab6c0fcc25d202416fee7c2ea88cc21b181639
Nuno <n@lopezi.com>
2024-06-21 14:15:08 +0100

    improve model option style

--- src/DeVinci_frontend/components/new/SelectModelOption.svelte
+++ src/DeVinci_frontend/components/new/SelectModelOption.svelte
@@ -10,11 +10,11 @@
 
 <li>
   <input type="radio" id={id} name={name} value={value} class="hidden peer" />
-  <label for={id} class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+  <label for={id} class="inline-flex items-center justify-between w-full p-3 text-gray-400 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
     <div class="block">
-      <div class="w-full text-lg font-semibold">{title}</div>
-      <div class="w-full text-md font-normal">{parameters}</div>
-      <span class="bg-blue-50 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">{performance}</span>
+      <div class="w-full text-gray-500 text-md font-semibold">{title}</div>
+      <div class="w-full text-sm font-normal">{parameters}</div>
+      <span class="text-blue-400 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">{performance}</span>
       <span class="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-400 border border-gray-500">{size}</span>
     </div>
     <svg class="w-5 h-5 ms-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
