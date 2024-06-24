85c2bf890189547e3f475fda58389af20bb6ad4f
patnorris <patrick@DESKTOP-TQ4V8LQ>
2024-06-24 17:32:10 +0200

    Hide knowledgebase for now

--- src/DeVinci_frontend/components/new/Sidebar.svelte
+++ src/DeVinci_frontend/components/new/Sidebar.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
   import SidebarHeader from "./SidebarHeader.svelte";
   import SidebarFooter from "./SidebarFooter.svelte";
-  import Knowledgebase from "./Knowledgebase.svelte";
+  //import Knowledgebase from "./Knowledgebase.svelte";
 </script>
 
 <div class="sidebar-header flex flex-col items-center justify-between py-4 h-lvh">
     <SidebarHeader />
     <SidebarFooter />
-    <Knowledgebase />
+    <!-- <Knowledgebase /> -->
 </div>
