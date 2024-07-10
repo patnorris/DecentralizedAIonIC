97ee8e072410d3641511ae7d6666931670e6c588
patnorris <patrick@DESKTOP-TQ4V8LQ>
2024-07-10 23:35:56 +0200

    Fix login

---
+++ src/DeVinci_frontend/components/new/LoginModal.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+  import InternetIdentityButton from "../InternetIdentityButton.svelte";
+  import NfidButton from "../NfidButton.svelte";
+  import BitfinityButton from "../BitfinityButton.svelte";
+  import PlugButton from "../PlugButton.svelte";
+
+  export let toggleModal;
+
+  let loading = ""; // this allows us to disable both buttons if one of them is clicked
+
+  // Main function to initialize modal functionality
+  const initializeModal = () => {
+    const modal = document.getElementById('crypto-modal');
+    // Add event listener to close the modal when clicking outside of it
+    window.addEventListener('click', (event) => {
+      if (event.target === modal) {
+        toggleModal();
+      }
+    });
+
+    // Close modal on 'Escape' key press
+    document.addEventListener('keydown', (event) => {
+      if (event.key === 'Escape') {
+        toggleModal();
+      }
+    });
+  };
+
+  onMount(() => {
+    initializeModal();
+  });
+</script>
+
+<div id="crypto-modal" tabindex="-1" aria-hidden="true" class="bg-gray-900/[.36] overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%)] max-h-full">
+  <div class="relative p-4 w-full mx-auto flex items-center content-center max-w-sm max-h-full h-full">
+    <!-- Modal content -->
+    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+      <!-- Modal header -->
+      <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+          Connect wallet
+        </h3>
+        <button type="button" on:click={() => {toggleModal()}} class="close-modal-button text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm h-8 w-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="crypto-modal">
+          <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+          </svg>
+          <span class="sr-only">Close modal</span>
+        </button>
+      </div>
+      <!-- Modal body -->
+      <div class="p-4 md:p-5">
+        <p class="text-sm font-normal text-gray-500 dark:text-gray-400">Connect with one of our available wallet providers.</p>
+        <ul class="my-4 space-y-3">
+          <li>
+            <InternetIdentityButton bind:loading {toggleModal} />
+          </li>
+          <li>
+            <NfidButton bind:loading {toggleModal} />
+          </li>
+          <li>
+            <BitfinityButton bind:loading {toggleModal} />
+          </li>
+          <li>
+            <PlugButton bind:loading {toggleModal} />
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
