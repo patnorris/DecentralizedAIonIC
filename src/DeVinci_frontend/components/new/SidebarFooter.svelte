4ea7331aac5747b65d50dc7a88859ea5562ee672
Nuno <n@lopezi.com>
2024-07-10 18:36:10 +0100

    re-position info panel on sidebar

--- src/DeVinci_frontend/components/new/SidebarFooter.svelte
+++ src/DeVinci_frontend/components/new/SidebarFooter.svelte
@@ -21,7 +21,7 @@
   <!--
   --- Show if user is not logged
   -->
-  <div id="dropdown-cta" class="p-4 mr-3 mb-20 absolute bottom-0 rounded-lg bg-blue-50 dark:bg-blue-900" role="alert">
+  <div id="dropdown-cta" class="p-4 mr-3 mb-4 absolute bottom-0 rounded-lg bg-blue-50 dark:bg-blue-900" role="alert">
     <div class="flex items-center mb-3">
       <span class="bg-green-200 text-blue-700 text-sm font-semibold me-2 px-2.5 py-0.5 rounded dark:bg-orange-200 dark:text-orange-900">Which model to choose?</span>
       <button id="close-button" type="button" class="ms-auto -mx-1.5 -my-1.5 bg-blue-50 inline-flex justify-center items-center w-6 h-6 text-blue-900 rounded-lg focus:ring-2 focus:ring-blue-400 p-1 hover:bg-blue-200 dark:bg-blue-900 dark:text-blue-400 dark:hover:bg-blue-800" data-dismiss-target="#dropdown-cta" aria-label="Close">
