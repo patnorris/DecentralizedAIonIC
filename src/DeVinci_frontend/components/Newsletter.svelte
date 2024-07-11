078766614f0c93a22968271d48a3d36ed76f2a39
Nuno <n@lopezi.com>
2024-07-11 11:32:00 +0100

    new style and structure for the /about page

---
+++ src/DeVinci_frontend/components/Newsletter.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import { location } from 'svelte-spa-router';
+  import { submitEmailSignUpForm } from "../helpers/utils.js";
+
+  let message = "";
+  const submitForm = async (event) => {
+    event.preventDefault();
+    // @ts-ignore
+    const email = document.getElementById('hero-email').value;
+    message = await submitEmailSignUpForm(email, $location);
+  };
+</script>
+
+<section class="bg-white dark:bg-gray-900">
+  <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+    <div class="mx-auto max-w-screen-md sm:text-center">
+      <h2 class="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl dark:text-white">Subscribe for deVinci newsletter</h2>
+      <p class="mx-auto mb-8 max-w-2xl  text-gray-500 md:mb-12 sm:text-xl dark:text-gray-400">Sign up with your email to stay up to date with the roadmap progress and announcements.</p>
+      <form on:submit={submitForm} >
+        <div class="items-center mx-auto mb-3 space-y-4 max-w-screen-sm sm:flex sm:space-y-0">
+          <div class="relative w-full">
+            <label for="hero-email" class="hidden mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email address</label>
+            <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+              <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
+                <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z"/>
+                <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z"/>
+              </svg>
+            </div>
+            <input id="hero-email" class="block p-3 pl-9 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 sm:rounded-none sm:rounded-l-lg focus:ring-primary-500 focus:border-primary-500" placeholder="Enter your email" type="email" required="">
+          </div>
+          <div>
+            <button type="submit" class="py-3 px-5 w-full text-sm font-medium text-center text-white rounded-lg border cursor-pointer bg-gray-700 border-gray-600 sm:rounded-none sm:rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300">Subscribe</button>
+          </div>
+        </div>
+        <div class="mx-auto max-w-screen-sm text-sm text-left text-gray-500 newsletter-form-footer dark:text-gray-300">deVinci 2024</div>
+      </form>
+      {#if message}
+        <h2>{message}</h2>
+      {/if}
+    </div>
+  </div>
+</section>
+
+<style>
+
+</style>
--- src/DeVinci_frontend/pages/About.svelte
+++ src/DeVinci_frontend/pages/About.svelte
@@ -1,26 +1,113 @@
 <script lang="ts">
-  import Topnav from "../components/Topnav.svelte";
-  import Footer from "../components/Footer.svelte";
-  import LoginMenu from "../components/LoginMenu.svelte";
+  import Sidebar from "../components/new/Sidebar.svelte";
+  import Navigation from "../components/new/Navigation.svelte";
+  import Newsletter from "../components/Newsletter.svelte";
+
+  import { onMount } from 'svelte';
+
+  import devincilogo from "/devinci-logo.svg";
+
+  onMount(() => {
+    const sidebarToggle = document.getElementById('sidebarToggle');
+    const chat = document.getElementById('chat');
+
+    function toggleSidebar(event) {
+      event.stopPropagation();
+      chat.classList.toggle('-translate-x-full');
+    }
+
+    function closeSidebar(event) {
+      if (!chat.contains(event.target) && !sidebarToggle.contains(event.target)) {
+        chat.classList.add('-translate-x-full');
+      }
+    }
+
+    function stopPropagation(event) {
+      event.stopPropagation();
+    }
+
+    sidebarToggle.addEventListener('click', toggleSidebar);
+    document.body.addEventListener('click', closeSidebar);
+    chat.addEventListener('click', stopPropagation);
+
+    return () => {
+      sidebarToggle.removeEventListener('click', toggleSidebar);
+      document.body.removeEventListener('click', closeSidebar);
+      chat.removeEventListener('click', stopPropagation);
+    };
+  });
+
 </script>
 
-<Topnav />
+<div class="flex flex-row h-screen">
+  <aside id="chat" class="fixed z-50 bg-gray-200 w-72 min-w-72 h-full md:shadow transform -translate-x-full md:translate-x-0 transition-transform duration-150 ease-in">
+    <div class="sidebar-content p-4 pt-0 h-full overflow-hidden">
+      <Sidebar />
+    </div>
+  </aside>
+  <main class="main flex flex-col flex-grow ml-0 md:ml-72 transition-all duration-150 ease-in">
+    <header class="header bg-white shadow py-2 px-4">
+      <div class="header-content flex items-center flex-row">
+        <!--
+        -- triggers sidebar on small devices
+        -->
+        <button id="sidebarToggle" data-drawer-target="chat" data-drawer-toggle="chat" aria-controls="chat" type="button" class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+          <span class="sr-only">Open sidebar</span>
+          <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+          </svg>
+        </button>
 
-<LoginMenu />
+        <div class="flex ml-auto">
+          <Navigation />
+        </div>
+      </div>
+    </header>
 
-<section id="about" class="py-7 space-y-6 items-center text-center bg-slate-100">
-  <h3 class="font-bold">Howdy from DeVinci</h3>
-  <p>Hi there! I'm DeVinci, your truly personal AI assistant. I can help you with whatever you need, and I'm always here to answer your questions. Whether you're looking for advice, support, or just a friendly chat, I'm here to help.</p>
-  <h3 class="font-bold">About DeVinci: Decentralized, Trusted, Open-source, Truly Yours</h3>
-  <p>DeVinci is your personalized AI assistant that redefines the paradigm of digital privacy and trust. It's not just an assistant; it's a mission to revolutionize how AI and data privacy intersect.</p>
-  <p>DeVinci is powered by an open-source AI model that operates directly within your browser. When you engage with DeVinci, the AI model is loaded and processed on your device. This means your interactions stay completely within the confines of your device. Your data never leaves your hands, ensuring unprecedented control over your data and a level of privacy that is a rarity in today's world of AI assistants.</p>
-  <p>Unlike traditional AI assistants backed by corporations, DeVinci is fully independent. There is no big company behind it, influencing or manipulating the responses to favor their interests. DeVinci's sole purpose is to serve you and provide you with the most honest, unbiased assistance.</p>
-  <p>For logged in users, DeVinci offers additional features such as persistent chats. By harnessing the power of the decentralized cloud, the Internet Computer, your chats are stored securely and privately. They can be retrieved and continued at any time, from any device.</p>
-  <p>In an era where data privacy and trustworthiness are highly valued, DeVinci stands at the forefront. It provides a personalized AI assistant experience that not only safeguards user privacy but also ensures unbiased, honest interactions. With DeVinci, you can have meaningful conversations, ask questions, seek help, and feel confident that your AI assistant truly has your best interests at heart.</p>
-  <p>We hope you enjoy your interactions with DeVinci and that it'll be highly useful to you as your favorite AI assistant. For any feedback or questions, please get in touch with us. Thank you for using our app and DeVinci!</p>
-  <h3 class="font-bold">Credits</h3>
-  <p>Running DeVinci in your browser is enabled by the great open-source project <a href='https://webllm.mlc.ai/' target='_blank' rel="noreferrer" class='underline'>Web LLM</a></p>
-  <p>Serving this app and hosting your data securely and in a decentralized way is made possible by the <a href='https://internetcomputer.org/' target='_blank' rel="noreferrer" class='underline'>Internet Computer</a></p>
-</section>
+    <main class="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-slate-100 dark:bg-gray-900 antialiased">
+      <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
+        <article class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+          <header class="mb-4 lg:mb-6 not-format">
+            <address class="flex items-center mb-6 not-italic">
+              <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
+                <img class="mr-4 w-16 h-16 rounded-full" src={devincilogo} alt="deVinci logo">
 
-<Footer />
+                <div>
+                  <a href="#" rel="author" class="text-xl font-bold text-gray-900 dark:text-white">deVinci</a>
+                  <p class="text-base text-gray-500 dark:text-gray-400"> Powered by <a href='https://www.onicai.com/' target='_blank' rel="noreferrer" class='underline'>onicai</a> and hosted on <a href='https://internetcomputer.org/' target='_blank' rel="noreferrer" class='underline'>Internet Computer</a>
+                  </p>
+                </div>
+              </div>
+            </address>
+            <h1 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">Howdy from deVinci</h1>
+          </header>
+          <p class="lead">Hi there! I'm deVinci, your truly personal AI assistant. I can help you with whatever you need, and I'm always here to answer your questions. Whether you're looking for advice, support, or just a friendly chat, I'm here to help.</p>
+          <h3 class="my-4 text-2xl font-bold dark:text-white">About deVinci<br /> Decentralized, Trusted, Open-source, Truly Yours</h3>
+          <p>DeVinci is your personalized AI assistant that redefines the paradigm of digital privacy and trust. It's not just an assistant; it's a mission to revolutionize how AI and data privacy intersect.</p>
+          <p class="my-4">DeVinci is powered by an open-source AI model that operates directly within your browser. When you engage with DeVinci, the AI model is loaded and processed on your device. This means your interactions stay completely within the confines of your device. Your data never leaves your hands, ensuring unprecedented control over your data and a level of privacy that is a rarity in today's world of AI assistants.</p>
+          <p class="my-4">Unlike traditional AI assistants backed by corporations, DeVinci is fully independent. There is no big company behind it, influencing or manipulating the responses to favor their interests. DeVinci's sole purpose is to serve you and provide you with the most honest, unbiased assistance.</p>
+          <p class="my-4">For logged in users, DeVinci offers additional features such as persistent chats. By harnessing the power of the decentralized cloud, the Internet Computer, your chats are stored securely and privately. They can be retrieved and continued at any time, from any device.</p>
+          <p class="my-4">In an era where data privacy and trustworthiness are highly valued, DeVinci stands at the forefront. It provides a personalized AI assistant experience that not only safeguards user privacy but also ensures unbiased, honest interactions. With DeVinci, you can have meaningful conversations, ask questions, seek help, and feel confident that your AI assistant truly has your best interests at heart.</p>
+          <p class="my-4">We hope you enjoy your interactions with DeVinci and that it'll be highly useful to you as your favorite AI assistant. For any feedback or questions, please get in touch with us. Thank you for using our app and DeVinci!</p>
+          <h3 class="font-bold">Credits</h3>
+          <p>Running DeVinci in your browser is enabled by the great open-source project <a href='https://webllm.mlc.ai/' target='_blank' rel="noreferrer" class='underline'>Web LLM</a></p>
+          <p>Serving this app and hosting your data securely and in a decentralized way is made possible by the <a href='https://internetcomputer.org/' target='_blank' rel="noreferrer" class='underline'>Internet Computer</a></p>
+        </article>
+      </div>
+    </main>
+
+    <Newsletter />
+
+  </main>
+</div>
+
+<style global>
+	.footer {
+		background: rgba(255,255,255,1);
+		padding-top: 10px;
+	}
+</style>
+
+
+
+
