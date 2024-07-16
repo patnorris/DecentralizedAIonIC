25fa41163795c8c3c491e40a2e61890f4cef1672
Nuno <n@lopezi.com>
2024-07-16 13:27:02 +0100

    style osmosis on login panel

--- src/DeVinci_frontend/components/new/LoginModal.svelte
+++ src/DeVinci_frontend/components/new/LoginModal.svelte
@@ -36,13 +36,13 @@
 <div id="crypto-modal" tabindex="-1" aria-hidden="true" class="bg-gray-900/[.36] overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%)] max-h-full">
   <div class="relative p-4 w-full mx-auto flex items-center content-center max-w-sm max-h-full h-full">
     <!-- Modal content -->
-    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+    <div class="relative bg-[lightsteelblue] rounded-lg shadow">
       <!-- Modal header -->
-      <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+      <div class="flex items-center justify-between p-4 pb-0 rounded-t">
+        <h3 class="text-lg font-semibold text-[#151b1e]">
           Connect wallet
         </h3>
-        <button type="button" on:click={() => {toggleModal()}} class="close-modal-button text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm h-8 w-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="crypto-modal">
+        <button type="button" on:click={() => {toggleModal()}} class="close-modal-button bg-blue-50 inline-flex justify-center items-center text-blue-900 rounded-lg focus:ring-2 focus:ring-blue-400 p-1 hover:bg-blue-200 hover:text-gray-900 text-sm h-8 w-8 ms-auto" data-modal-toggle="crypto-modal">
           <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
           </svg>
@@ -51,7 +51,7 @@
       </div>
       <!-- Modal body -->
       <div class="p-4 md:p-5">
-        <p class="text-sm font-normal text-gray-500 dark:text-gray-400">Connect with one of our available wallet providers.</p>
+        <p class="text-sm font-normal text-[#151b1e]">Connect with one of our available wallet providers.</p>
         <ul class="my-4 space-y-3">
           <li>
             <InternetIdentityButton bind:loading {toggleModal} />
--- src/DeVinci_frontend/components/new/SidebarFooter.svelte
+++ src/DeVinci_frontend/components/new/SidebarFooter.svelte
@@ -22,7 +22,7 @@
   --- Show if user is not logged
   -->
 
-  <div id="dropdown-cta" class="p-4 mb-4 text-[#151b1e] border-2 border-dotted border-[#151b1e] rounded-2xl bg-[#f9c490]" role="alert">
+  <div id="dropdown-cta" class="p-4 mb-4 text-[#151b1e] border-2 border-dotted border-[#151b1e] rounded-2xl bg-[lightsteelblue]" role="alert">
     <div class="flex items-center mb-3">
       <svg class="flex-shrink-0 w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
         <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
