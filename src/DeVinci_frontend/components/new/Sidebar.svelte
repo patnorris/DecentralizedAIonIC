f3ff6e036b715241eb4de78168bcd4fc7ccba5a1
patnorris <patrick@DESKTOP-TQ4V8LQ>
2024-07-28 19:07:22 +0200

    Hide sidebar footer if user has downloaded at least one model

--- src/DeVinci_frontend/components/new/Sidebar.svelte
+++ src/DeVinci_frontend/components/new/Sidebar.svelte
@@ -2,10 +2,17 @@
   import SidebarHeader from "./SidebarHeader.svelte";
   import SidebarFooter from "./SidebarFooter.svelte";
   //import Knowledgebase from "./Knowledgebase.svelte";
+
+  import { userHasDownloadedModel } from "../../helpers/localStorage";
+
+  // Reactive statement to check if the user has already downloaded at least one AI model
+  $: userHasDownloadedAtLeastOneModel = userHasDownloadedModel();
 </script>
 
 <div class="sidebar-header flex flex-col items-center justify-between py-4 h-lvh">
     <SidebarHeader />
-    <SidebarFooter />
+    {#if !userHasDownloadedAtLeastOneModel}
+      <SidebarFooter />
+    {/if}
     <!-- <Knowledgebase /> -->
 </div>
