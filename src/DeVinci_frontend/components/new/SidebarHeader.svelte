6a2a1093a2a3354d28f07d4bf8c5658dc4b3aae8
Nuno <n@lopezi.com>
2024-07-29 15:44:50 +0100

    choose a model tooltip before chat enabled

--- src/DeVinci_frontend/components/new/SidebarHeader.svelte
+++ src/DeVinci_frontend/components/new/SidebarHeader.svelte
@@ -31,7 +31,7 @@
       New chat
     </button>
   {:else}
-    <button disabled type="button" on:click={showNewChat} class="disabled:bg-gray-100 disabled:cursor-not-allowed disabled:text-gray-800 mr-auto w-full my-5 flex justify-center text-gray-800 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-400 font-medium rounded-full text-xs px-3 py-1.5 text-center">
+    <button disabled aria-label="Choose a model first." type="button" on:click={showNewChat} class="tooltip-toggle disabled:bg-gray-100 disabled:cursor-not-allowed disabled:text-gray-800 mr-auto w-full my-5 flex justify-center text-gray-800 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-400 font-medium rounded-full text-xs px-3 py-1.5 text-center">
       New chat
     </button>
   {/if}
@@ -64,5 +64,46 @@
 	  border-radius: 50%; /* rounded-full in Tailwind CSS */
 	  background-color: #f9fafb; /* bg-gray-50 in Tailwind CSS */
 	  animation: rotate 360s linear infinite;
+  }
+
+  
+  .tooltip-toggle {
+	  cursor: pointer;
+	  position: relative;
+  }
+  .tooltip-toggle::before {
+	  position: absolute;
+	  top: -60px;
+	  left: 0;
+	  background-color: #2B222A;
+	  border-radius: 5px;
+	  color: #fff;
+	  content: attr(aria-label);
+	  padding: 1rem;
+	  text-transform: none;
+	  transition: all 0.5s ease;
+	  width: 140px;
+  }
+  .tooltip-toggle::after {
+	  position: absolute;
+	  top: -12px;
+	  left: 9px;
+	  content: " ";
+	  font-size: 0;
+	  line-height: 0;
+	  margin-left: -5px;
+	  width: 0;
+  }
+  .tooltip-toggle::before, .tooltip-toggle::after {
+	  color: #efefef;
+	  font-family: monospace;
+	  font-size: 12px;
+	  opacity: 0;
+	  pointer-events: none;
+	  text-align: center;
+  }
+  .tooltip-toggle:focus::before, .tooltip-toggle:focus::after, .tooltip-toggle:hover::before, .tooltip-toggle:hover::after {
+	  opacity: 1;
+	  transition: all 0.75s ease;
   }
 </style>
