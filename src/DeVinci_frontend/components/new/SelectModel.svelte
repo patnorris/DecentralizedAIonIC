bdb9c269f096e19e9892366395ec69591d325b4c
Nuno <n@lopezi.com>
2024-07-29 15:34:00 +0100

    add select model info panel

--- src/DeVinci_frontend/components/new/SelectModel.svelte
+++ src/DeVinci_frontend/components/new/SelectModel.svelte
@@ -19,32 +19,37 @@
 
 </script>
 
-  <ul class="grid w-full gap-2 md:grid-cols-2 px-4 mt-4">
-    {#each availableAiModels as option}
-      {#if userHasDownloadedAtLeastOneModel}
-        <SelectModelOption
-          id={option.id}
-          name="{option.name}"
-          value={option.value}
-          title={option.title}
-          parameters={option.parameters}
-          performance={option.performance}
-          size={option.size}
-          bind:chatModelDownloadInProgress
-          onlyShowIfDownloaded={onlyShowDownloadedModels}
-          autoInitiateIfModelSelected={autoInitiateSelectedModel}
-        />
-      {:else}
-        <SelectModelOption
-          id={option.id}
-          name="{option.name}"
-          value={option.value}
-          title={option.title}
-          parameters={option.parameters}
-          performance={option.performance}
-          size={option.size}
-          bind:chatModelDownloadInProgress
-        />
-      {/if}
-    {/each}
-  </ul>
+  <div>
+    <div class="m-4 mt-2 px-4 py-3 text-gray-500 inline-block border border-[#b0c4de] rounded-lg sm:px-5 bg-gray-50">
+      <b>Please select a model</b>. When selecting an AI model to interact with, consider the balance between performance and hardware requirements. Larger models generally offer enhanced accuracy and advanced features, making them ideal for complex tasks. However, they demand more powerful hardware and may operate slower.
+    </div>
+    <ul class="grid w-full gap-2 md:grid-cols-2 px-4 mt-4">
+      {#each availableAiModels as option}
+        {#if userHasDownloadedAtLeastOneModel}
+          <SelectModelOption
+            id={option.id}
+            name="{option.name}"
+            value={option.value}
+            title={option.title}
+            parameters={option.parameters}
+            performance={option.performance}
+            size={option.size}
+            bind:chatModelDownloadInProgress
+            onlyShowIfDownloaded={onlyShowDownloadedModels}
+            autoInitiateIfModelSelected={autoInitiateSelectedModel}
+          />
+        {:else}
+          <SelectModelOption
+            id={option.id}
+            name="{option.name}"
+            value={option.value}
+            title={option.title}
+            parameters={option.parameters}
+            performance={option.performance}
+            size={option.size}
+            bind:chatModelDownloadInProgress
+          />
+        {/if}
+      {/each}
+    </ul>
+  </div>
