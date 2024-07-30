9fabbe689ef63316a1e42a638333064cbcc4ef20
Nuno <n@lopezi.com>
2024-07-30 15:01:42 +0100

    improve chat hint panels

--- src/DeVinci_frontend/components/new/StartUpChatPanel.svelte
+++ src/DeVinci_frontend/components/new/StartUpChatPanel.svelte
@@ -7,22 +7,22 @@
       <img src={devincilogo} class="rotating-image w-16 h-16 p-0 m-8 rounded-full" alt="devinci logo" />
     <div class="grid grid-cols-1 gap-4 sm:grid-cols-4 w-full">
       <div class="text-[#151b1e] bg-gray-100 border-2 border-dotted border-[#f9c490] rounded-lg">
-        <div class="inline-flex items-center justify-between w-full p-3 peer-checked:border-solid peer-checked:cursor-default peer-checked:bg-[lightsteelblue] peer-checked:border-[#151b1e] peer-checked:text-[#151b1e] hover:text-gray-600 hover:bg-gray-50">
+        <div class="inline-flex items-center justify-between w-full p-3">
           <p class="w-full text-[#151b1e] text-sm font-normal">Recommend a good book</p>
         </div>
       </div>
       <div class="text-[#151b1e] bg-gray-100 border-2 border-dotted border-[#a1c490] rounded-lg">
-        <div class="inline-flex items-center justify-between w-full p-3 peer-checked:border-solid peer-checked:cursor-default peer-checked:bg-[lightsteelblue] peer-checked:border-[#151b1e] peer-checked:text-[#151b1e] hover:text-gray-600 hover:bg-gray-50">
+        <div class="inline-flex items-center justify-between w-full p-3">
           <p class="w-full text-[#151b1e] text-sm font-normal">Explain quantum physics</p>
         </div>
       </div>
       <div class="text-[#151b1e] bg-gray-100 border-2 border-dotted border-[#f0e68c] rounded-lg">
-        <div class="inline-flex items-center justify-between w-full p-3 peer-checked:border-solid peer-checked:cursor-default peer-checked:bg-[lightsteelblue] peer-checked:border-[#151b1e] peer-checked:text-[#151b1e] hover:text-gray-600 hover:bg-gray-50">
+        <div class="inline-flex items-center justify-between w-full p-3">
           <p class="w-full text-[#151b1e] text-sm font-normal">Share a fun fact</p>
         </div>
       </div>
         <div class="text-[#151b1e] bg-gray-100 border-2 border-dotted border-[#cb8bd0] rounded-lg">
-            <div class="inline-flex items-center justify-between w-full p-3 peer-checked:border-solid peer-checked:cursor-default peer-checked:bg-[lightsteelblue] peer-checked:border-[#151b1e] peer-checked:text-[#151b1e] hover:text-gray-600 hover:bg-gray-50">
+            <div class="inline-flex items-center justify-between w-full p-3">
                 <p class="w-full text-[#151b1e] text-sm font-normal">How to cook pasta?</p>
             </div>
         </div>
