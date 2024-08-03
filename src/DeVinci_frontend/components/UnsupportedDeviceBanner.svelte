29a3bb4552394da615cba5c158b75b5d58683cbb
Nuno <n@lopezi.com>
2024-08-03 21:52:24 +0100

    unsupported ui

--- src/DeVinci_frontend/components/UnsupportedDeviceBanner.svelte
+++ src/DeVinci_frontend/components/UnsupportedDeviceBanner.svelte
@@ -5,4 +5,3 @@
 <div style="background-color: orange; padding: 1em; color: white; margin-bottom: 1em;">
 Unfortunately, your device doesn't support the features needed for DeVinci yet. Please use a desktop computer and one of the following browsers instead: {webGpuSupportedBrowsers}
 </div>
-  
--- src/DeVinci_frontend/pages/deVinci.svelte
+++ src/DeVinci_frontend/pages/deVinci.svelte
@@ -2,8 +2,11 @@
   import Sidebar from "../components/new/Sidebar.svelte";
   import Navigation from "../components/new/Navigation.svelte";
   import ChatInterface from "../components/new/ChatInterface.svelte";
+  import UnsupportedBrowserBanner from "../components/new/UnsupportedBrowserBanner.svelte";
+  import UnsupportedDeviceBanner from "../components/new/UnsupportedDeviceBanner.svelte";
 
   import { onMount } from 'svelte';
+  import { deviceType, supportsWebGpu } from "../store";
 
   onMount(() => {
     const sidebarToggle = document.getElementById('sidebarToggle');
@@ -34,36 +37,42 @@
       chat.removeEventListener('click', stopPropagation);
     };
   });
-
 </script>
 
-<div class="flex flex-row h-screen">
-  <aside id="chat" class="fixed z-50 bg-gray-200 w-72 min-w-72 h-full md:shadow transform -translate-x-full md:translate-x-0 transition-transform duration-150 ease-in">
-    <div class="sidebar-content p-4 pt-0 h-full overflow-hidden">
-      <Sidebar />
-    </div>
-  </aside>
-  <main class="main flex flex-col flex-grow ml-0 md:ml-72 transition-all duration-150 ease-in">
-    <header class="header bg-white shadow py-2 px-4">
-      <div class="header-content flex items-center flex-row">
-        <!--
-        -- triggers sidebar on small devices
-        -->
-        <button id="sidebarToggle" data-drawer-target="chat" data-drawer-toggle="chat" aria-controls="chat" type="button" class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-          <span class="sr-only">Open sidebar</span>
-          <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-          </svg>
-        </button>
-
-        <div class="flex ml-auto">
-          <Navigation />
-        </div>
+{#if !(deviceType === 'desktop' || deviceType === 'Android')}
+  <UnsupportedDeviceBanner />
+{:else if !supportsWebGpu}
+  <UnsupportedBrowserBanner />
+{:else}
+  <div class="flex flex-row h-screen">
+    <aside id="chat" class="fixed z-50 bg-gray-200 w-72 min-w-72 h-full md:shadow transform -translate-x-full md:translate-x-0 transition-transform duration-150 ease-in">
+      <div class="sidebar-content p-4 pt-0 h-full overflow-hidden">
+        <Sidebar />
       </div>
-    </header>
-    <ChatInterface />
-  </main>
-</div>
+    </aside>
+    <main class="main flex flex-col flex-grow ml-0 md:ml-72 transition-all duration-150 ease-in">
+      <header class="header bg-white shadow py-2 px-4">
+        <div class="header-content flex items-center flex-row">
+          <!--
+          -- triggers sidebar on small devices
+          -->
+          <button id="sidebarToggle" data-drawer-target="chat" data-drawer-toggle="chat" aria-controls="chat" type="button" class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+            <span class="sr-only">Open sidebar</span>
+            <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+            </svg>
+          </button>
+          <div class="flex ml-auto">
+            <Navigation />
+          </div>
+        </div>
+      </header>
+      <ChatInterface />
+    </main>
+  </div>
+{/if}
+
+
 
 <style global>
   .footer {
