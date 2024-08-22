19be954200165004b31c790bdf14089f5fd550ed
Nuno <n@lopezi.com>
2024-08-22 15:52:06 +0100

    optimise computation

--- src/DeVinci_frontend/components/Navigation.svelte
+++ src/DeVinci_frontend/components/Navigation.svelte
@@ -78,8 +78,7 @@
       <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 4 15">
         <path d="M3.5 1.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 6.041a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 5.959a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"/>
       </svg>
-    {/if}
-    {#if $store.isAuthed}
+    {:else}
       <div class="w-10 h-10 -m-2.5 rounded-lg bg-violet-500 text-gray-100 flex justify-center items-center content-center">0X</div>
     {/if}
   </button>
