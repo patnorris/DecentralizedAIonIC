d6962cacb2024c893207057d3947c557f92a7827
Nuno <n@lopezi.com>
2024-08-22 10:19:03 +0100

    add user logged indicator

--- src/DeVinci_frontend/components/Navigation.svelte
+++ src/DeVinci_frontend/components/Navigation.svelte
@@ -72,12 +72,17 @@
   </button>
 {/if}
 
-
 <div class="relative inline-block">
   <button id="dropdownMenuIconButton" data-dropdown-toggle="dropdownDots" class="inline-flex items-center p-2.5 text-sm font-medium text-center text-gray-900 bg-white rounded-lg hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100" type="button">
-    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 4 15">
-      <path d="M3.5 1.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 6.041a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 5.959a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"/>
-    </svg>
+
+    {#if !$store.isAuthed}
+      <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 4 15">
+        <path d="M3.5 1.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 6.041a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 5.959a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"/>
+      </svg>
+    {/if}
+    {#if $store.isAuthed}
+      <div class="w-10 h-10 -m-2.5 rounded-lg bg-violet-500 text-gray-100 flex justify-center items-center content-center">0X</div>
+    {/if}
   </button>
 
   <!-- Dropdown menu -->
