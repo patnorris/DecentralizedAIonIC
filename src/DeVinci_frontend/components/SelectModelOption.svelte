6958558cb12acbf4fcaac372fd5aaf39feadf86b
patnorris <patrick@DESKTOP-TQ4V8LQ>
2024-08-23 17:46:37 +0200

    Indicate model download right after click

--- src/DeVinci_frontend/components/SelectModelOption.svelte
+++ src/DeVinci_frontend/components/SelectModelOption.svelte
@@ -142,6 +142,9 @@
     };
     console.log("Loading chat model...");
     chatModelDownloadInProgress = true;
+    downloadText = "Downloading... please wait.";
+    initiateText = "Initiating... please wait.";
+
     if (!modelOptionId) {
       modelOptionId = $selectedAiModelId;
     };
@@ -241,8 +244,8 @@
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8.032 12 1.984 1.984 4.96-4.96m4.55 5.272.893-.893a1.984 1.984 0 0 0 0-2.806l-.893-.893a1.984 1.984 0 0 1-.581-1.403V7.04a1.984 1.984 0 0 0-1.984-1.984h-1.262a1.983 1.983 0 0 1-1.403-.581l-.893-.893a1.984 1.984 0 0 0-2.806 0l-.893.893a1.984 1.984 0 0 1-1.403.581H7.04A1.984 1.984 0 0 0 5.055 7.04v1.262c0 .527-.209 1.031-.581 1.403l-.893.893a1.984 1.984 0 0 0 0 2.806l.893.893c.372.372.581.876.581 1.403v1.262a1.984 1.984 0 0 0 1.984 1.984h1.262c.527 0 1.031.209 1.403.581l.893.893a1.984 1.984 0 0 0 2.806 0l.893-.893a1.985 1.985 0 0 1 1.403-.581h1.262a1.984 1.984 0 0 0 1.984-1.984V15.7c0-.527.209-1.031.581-1.403Z"/>
           </svg>
         </span>
-      {:else if downloadProgress}
-        {#if downloadProgress !== 0}
+      {:else}
+        {#if downloadProgress}
           <div class="w-full bg-gray-200 my-1 rounded-full">
             <div class="bg-[dimgrey] text-xs font-medium text-orange-50 text-center p-0.5 leading-none rounded-full" style="width: {downloadProgress}%;">
               {downloadProgress}%
