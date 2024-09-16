e16508500338f8e09916c7f2d3e31972d028680b
Nuno <n@lopezi.com>
2024-09-16 10:11:11 +0100

    smaller toast

--- src/DeVinci_frontend/components/InstallToastNotification.svelte
+++ src/DeVinci_frontend/components/InstallToastNotification.svelte
@@ -39,7 +39,7 @@
 </script>
 
 {#if showToast}
-  <div class="toast flex items-center w-full max-w-md p-8 text-[#151b1e] bg-[lightsteelblue] rounded-lg shadow" role="alert">
+  <div class="toast flex items-center w-full max-w-md p-4 text-[#151b1e] bg-[lightsteelblue] rounded-lg shadow" role="alert">
     <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
       <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm9.408-5.5a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2h-.01ZM10 10a1 1 0 1 0 0 2h1v3h-1a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2h-1v-4a1 1 0 0 0-1-1h-2Z" clip-rule="evenodd"/>
     </svg>
