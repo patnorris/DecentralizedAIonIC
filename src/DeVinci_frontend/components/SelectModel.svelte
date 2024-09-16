38fef9b5fdc380c92c0236f677afe0a61f573f8d
Nuno <n@lopezi.com>
2024-09-16 10:18:17 +0100

    replace message for download info. add icon

--- src/DeVinci_frontend/components/SelectModel.svelte
+++ src/DeVinci_frontend/components/SelectModel.svelte
@@ -21,7 +21,10 @@
 
   <div>
     <div class="m-4 mt-2 px-4 py-3 text-gray-500 inline-block border border-[#b0c4de] rounded-lg sm:px-5 bg-gray-50">
-      <b>Please select a model</b>. When choosing an AI model, balance performance with hardware needs. Larger models offer better accuracy and features but require more powerful hardware and may run slower.
+      <svg class="flex-shrink-0 inline w-4 h-4 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+      </svg>
+      <b>When you select an LLM model</b>, it will download to your browser cache and will need to be started afterward. This ensures 100% privacy, but please note that the download process might take some time.
     </div>
     <ul class="grid w-full gap-2 md:grid-cols-2 px-4 mt-4">
       {#each availableAiModels as option}
