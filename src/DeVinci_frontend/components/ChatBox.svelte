3b5944760cce69fd40da53bd84047e3ba6a12db4
Nuno <n@lopezi.com>
2024-10-02 00:09:36 +0100

    simplify interruptMessageGeneration method

--- src/DeVinci_frontend/components/ChatBox.svelte
+++ src/DeVinci_frontend/components/ChatBox.svelte
@@ -43,26 +43,17 @@
 		return { update: scroll }
 	};
 
-  async function showNewChat() {
-    if ($activeChatGlobal !== null) {
-      $activeChatGlobal = null;
-    } else {
-      $activeChatGlobal = false;
-    }
-    if ($chatModelGlobal) {
-      try {
-        await $chatModelGlobal.interruptGenerate();
-      } catch (error) {
-        console.error("Error stopping the answer generation on starting new chat ", error);
-      }
-    }
-    messages = [];
-    newMessageText = '';
-    messageGenerationInProgress = false;
-    if (location.hash !== "#/") {
-      push('/');
+  async function interruptMessageGeneration() {
+  if ($chatModelGlobal) {
+    try {
+      await $chatModelGlobal.interruptGenerate();
+      console.log("Message generation interrupted successfully");
+    } catch (error) {
+      console.error("Error stopping the answer generation:", error);
     }
   }
+  messageGenerationInProgress = false;
+}
 
 // Whether user wants their messages to be stored
   let saveChats = getLocalFlag("saveChatsUserSelection"); // default is save
@@ -299,7 +290,7 @@
         </button>
       {:else if messageGenerationInProgress}
         <input bind:value={newMessageText} type="text" id="chat" class="block mx-4 p-3 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-2 focus:outline-none focus:ring-[#24292F]/50 " />
-        <button on:click={showNewChat} type="submit" class="inline-flex justify-center p-2 text-gray-600 rounded-full bg-gray-100 hover:bg-gray-300">
+        <button on:click={interruptMessageGeneration} type="submit" class="inline-flex justify-center p-2 text-gray-600 rounded-full bg-gray-100 hover:bg-gray-300">
           <svg class="w-6 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
             <path d="M7 5a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2H7Z"/>
           </svg>
