127818b8b5405bf571b82a64b03d4e9d07f2e00c
patnorris <patrick@DESKTOP-TQ4V8LQ>
2024-11-02 17:30:34 +0100

    Remove pdf only upload

--- src/DeVinci_frontend/pages/Knowledgebase.svelte
+++ src/DeVinci_frontend/pages/Knowledgebase.svelte
@@ -129,18 +129,6 @@
     </button>
   </div>
   <div class="flex items-center px-3 p-2 rounded-full bg-gray-200">
-    <label for="pdf_chat">
-      <button type="button" on:click={() => document.getElementById('pdf_chat').click()} class="inline-flex justify-center p-2 text-gray-500 rounded-full cursor-pointer hover:text-gray-900 hover:bg-gray-100">
-        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M720-330q0 104-73 177T470-80q-104 0-177-73t-73-177v-370q0-75 52.5-127.5T400-880q75 0 127.5 52.5T580-700v350q0 46-32 78t-78 32q-46 0-78-32t-32-78v-370h80v370q0 13 8.5 21.5T470-320q13 0 21.5-8.5T500-350v-350q-1-42-29.5-71T400-800q-42 0-71 29t-29 71v370q-1 71 49 120.5T470-160q70 0 119-49.5T640-330v-390h80v390Z"/></svg>
-      </button>
-    </label>
-    <input id="pdf_chat" type="file" accept=".pdf" on:change={uploadPdfToKnowledgeBase} class="hidden text-sm text-gray-900 border border-gray-300 cursor-pointer bg-gray-50 ml-2">
-    {#if loadingKnowledgeDatabase}
-      <p class="font-semibold text-gray-900">Loading the content into your Knowledge Base for you...</p>
-      <img class="h-12 mx-auto p-1 block" src={spinner} alt="loading animation" />
-    {/if}
-  </div>
-  <div class="flex items-center px-3 p-2 rounded-full bg-gray-200">
     <label for="file_upload">
       <button type="button" on:click={() => document.getElementById('file_upload').click()} class="inline-flex justify-center p-2 text-gray-500 rounded-full cursor-pointer hover:text-gray-900 hover:bg-gray-100">
         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M720-330q0 104-73 177T470-80q-104 0-177-73t-73-177v-370q0-75 52.5-127.5T400-880q75 0 127.5 52.5T580-700v350q0 46-32 78t-78 32q-46 0-78-32t-32-78v-370h80v370q0 13 8.5 21.5T470-320q13 0 21.5-8.5T500-350v-350q-1-42-29.5-71T400-800q-42 0-71 29t-29 71v370q-1 71 49 120.5T470-160q70 0 119-49.5T640-330v-390h80v390Z"/></svg>
