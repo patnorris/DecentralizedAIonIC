0ca1fb23bbf1e23bc3c76a341fa5c67bb2d48e38
patnorris <patrick@DESKTOP-TQ4V8LQ>
2024-11-13 11:57:01 +0100

    Updates before merging

---
+++ src/arcmindvectordb/interact/add-vec-test-knowledgebase.sh
@@ -0,0 +1,8 @@
+# To deplopy locally; update IC_NETWORK to local. To deploy to ic; update IC_NETWORK to ic.
+
+CONTENT_TEXT="$(cat embeddings/investment2-content.txt)"
+CONTENT_VECTOR="$(cat embeddings/investment2-vec.txt)"
+
+echo CONTENT_TEXT=$CONTENT_TEXT
+echo CONTENT_VECTOR=$CONTENT_VECTOR
+dfx canister call --network local b77ix-eeaaa-aaaaa-qaada-cai add "(record {content = \"$CONTENT_TEXT\"; embeddings = vec $CONTENT_VECTOR})"
