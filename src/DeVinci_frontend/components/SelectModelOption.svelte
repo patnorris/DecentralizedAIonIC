a77a1f99cb8543ccdc925bc4a85c6430a3817224
Nuno Lopes <n@lopezi.com>
2024-12-04 19:13:00 +0000

    update copyright for better user feedback

--- src/DeVinci_frontend/components/SelectModelOption.svelte
+++ src/DeVinci_frontend/components/SelectModelOption.svelte
@@ -303,11 +303,10 @@
               on:mouseenter={() => showLanguagesTooltip = true}
               on:mouseleave={() => showLanguagesTooltip = false}
             >
-              <svg xmlns="http://www.w3.org/2000/svg" class="w-2.5 h-2.5 mr-0.5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                <circle cx="12" cy="12" r="10"/>
-                <path d="M2 12h20M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"/>
+              Good for
+              <svg xmlns="http://www.w3.org/2000/svg" class="w-3 h-3 ml-0.5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                <path d="M18 6L6 18M18 6h-8M18 6v8" stroke-linecap="round" stroke-linejoin="round"/>
               </svg>
-              Supported Languages
             </span>
             {#if showLanguagesTooltip}
               <div class="absolute left-1/2 transform -translate-x-1/2 top-full mt-2 z-50 w-48 p-2 text-sm text-white bg-black rounded-lg shadow-lg">
