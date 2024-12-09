f2368b8c36f1da1374345f314bb55ce8192e510e
Nuno <n@lopezi.com>
2024-12-09 19:33:06 +0000

    remove KB from bottom nav. update meta url.

--- src/DeVinci_frontend/components/SidebarBottomNav.svelte
+++ src/DeVinci_frontend/components/SidebarBottomNav.svelte
@@ -8,14 +8,14 @@
   </script>
   
   <div class="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200">
-    <div class="grid h-full max-w-lg grid-cols-2 mx-auto font-medium">
+    <div class="grid h-full max-w-lg grid-cols-1 mx-auto font-medium">
       <a href="#/settings" class="border inline-flex flex-col items-center justify-center px-1 hover:bg-gray-50 group">
         <span class="text-[10px] text-gray-500 group-hover:text-[#cb8bd0] text-left tracking-tight">
           CURRENT MODEL<br/>{$currentModelName}
         </span>
       </a>
       
-      <button type="button" class="border inline-flex flex-col items-center justify-center px-1 hover:bg-gray-50 group">
+      <button type="button" class="border hidden inline-flex flex-col items-center justify-center px-1 hover:bg-gray-50 group">
         <label class="inline-flex items-center cursor-pointer">
           <span class="text-[10px] font-medium text-gray-500 text-left mr-2 uppercase tracking-tight">Knowledge<br /> Base</span>
           <input type="checkbox" bind:checked={$useKnowledgeBase} on:change={handleChange} class="sr-only peer">
