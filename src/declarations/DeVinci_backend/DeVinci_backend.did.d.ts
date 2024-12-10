8e47a8d79f8ff3382da832fc8fbde3bb8a20ed66
patnorris <patrick@DESKTOP-TQ4V8LQ>
2024-12-10 12:22:45 +0100

    Add updated declarations

--- src/declarations/DeVinci_backend/DeVinci_backend.did.d.ts
+++ src/declarations/DeVinci_backend/DeVinci_backend.did.d.ts
@@ -6,26 +6,6 @@ export type ApiError = { 'ZeroAddress' : null } |
   { 'InvalidTokenId' : null } |
   { 'Unauthorized' : null } |
   { 'Other' : string };
-export interface AuthRecord { 'auth' : string }
-export type AuthRecordResult = { 'Ok' : AuthRecord } |
-  { 'Err' : ApiError };
-export interface AvailableCanistersRecord { 'canisterType' : CanisterType }
-export interface CanisterCreationConfigurationInput {
-  'canisterType' : CanisterType,
-}
-export interface CanisterCreationRecord {
-  'creationResult' : string,
-  'newCanisterId' : string,
-}
-export type CanisterCreationResult = { 'Ok' : CanisterCreationRecord } |
-  { 'Err' : ApiError };
-export interface CanisterInfo {
-  'canisterType' : CanisterType,
-  'creationTimestamp' : bigint,
-  'canisterAddress' : string,
-}
-export type CanisterType = { 'Knowledgebase' : null } |
-  { 'Backend' : null };
 export interface Chat {
   'id' : string,
   'messages' : Array<Message>,
@@ -51,85 +31,35 @@ export type ChatsPreviewResult = { 'Ok' : Array<ChatPreview> } |
 export type ChatsResult = { 'Ok' : Array<Chat> } |
   { 'Err' : ApiError };
 export interface DeVinciBackend {
-  'add_to_user_knowledgebase' : ActorMethod<
-    [string, Embeddings],
-    MemoryVectorsStoredResult
-  >,
-  'amiController' : ActorMethod<[], AuthRecordResult>,
-  'check_caller_has_memory_vectors_entry' : ActorMethod<
-    [],
-    MemoryVectorsCheckResult
-  >,
-  'createNewCanister' : ActorMethod<
-    [CanisterCreationConfigurationInput],
-    CanisterCreationResult
-  >,
   'create_chat' : ActorMethod<[Array<Message>], ChatCreationResult>,
   'delete_chat' : ActorMethod<[string], ChatResult>,
   'delete_email_subscriber' : ActorMethod<[string], boolean>,
-  'getUserCanistersEntry' : ActorMethod<
-    [AvailableCanistersRecord],
-    UserCanistersEntryResult
-  >,
   'get_caller_chat_history' : ActorMethod<[], ChatsPreviewResult>,
   'get_caller_chats' : ActorMethod<[], ChatsResult>,
-  'get_caller_memory_vectors' : ActorMethod<[], MemoryVectorsResult>,
   'get_caller_settings' : ActorMethod<[], UserSettingsResult>,
   'get_chat' : ActorMethod<[string], ChatResult>,
   'get_email_subscribers' : ActorMethod<[], Array<[string, EmailSubscriber]>>,
   'greet' : ActorMethod<[string], string>,
-  'isControllerLogicOk' : ActorMethod<[], AuthRecordResult>,
-  'migrate_user_chats' : ActorMethod<[Principal, List], boolean>,
-  'search_user_knowledgebase' : ActorMethod<
-    [Embeddings],
-    SearchKnowledgeBaseResult
-  >,
-  'setCanisterCreationCanisterId' : ActorMethod<[string], AuthRecordResult>,
-  'store_user_chats_memory_vectors' : ActorMethod<
-    [Array<MemoryVector>],
-    MemoryVectorsStoredResult
-  >,
   'submit_signup_form' : ActorMethod<[SignUpFormInput], string>,
-  'updateCanisterIsPrivate' : ActorMethod<[boolean], boolean>,
   'update_caller_settings' : ActorMethod<
     [UserSettings],
     UpdateUserSettingsResult
   >,
   'update_chat_messages' : ActorMethod<[string, Array<Message>], ChatIdResult>,
   'update_chat_metadata' : ActorMethod<[UpdateChatObject], ChatIdResult>,
-  'whoami' : ActorMethod<[], Principal>,
 }
 export interface EmailSubscriber {
   'subscribedAt' : bigint,
   'emailAddress' : string,
   'pageSubmittedFrom' : string,
 }
-export type Embeddings = Array<number>;
-export type List = [] | [[Chat, List]];
-export interface MemoryVector {
-  'content' : string,
-  'metadata' : MemoryVectorMetadata,
-  'embedding' : Embeddings,
-}
-export interface MemoryVectorMetadata { 'id' : bigint }
-export type MemoryVectorsCheckResult = { 'Ok' : boolean } |
-  { 'Err' : ApiError };
-export type MemoryVectorsResult = { 'Ok' : Array<MemoryVector> } |
-  { 'Err' : ApiError };
-export type MemoryVectorsStoredResult = { 'Ok' : boolean } |
-  { 'Err' : ApiError };
 export interface Message { 'content' : string, 'sender' : string }
-export type SearchKnowledgeBaseResult = { 'Ok' : string } |
-  { 'Err' : ApiError };
 export interface SignUpFormInput {
   'emailAddress' : string,
   'pageSubmittedFrom' : string,
 }
 export interface UpdateChatObject { 'id' : string, 'chatTitle' : string }
 export type UpdateUserSettingsResult = { 'Ok' : boolean } |
-  { 'Err' : ApiError };
-export interface UserCanisterEntry { 'userCanister' : CanisterInfo }
-export type UserCanistersEntryResult = { 'Ok' : UserCanisterEntry } |
   { 'Err' : ApiError };
 export interface UserSettings {
   'responseLength' : string,
--- src/declarations/DeVinci_backend/DeVinci_backend.did.js
+++ src/declarations/DeVinci_backend/DeVinci_backend.did.js
@@ -1,38 +1,11 @@
 export const idlFactory = ({ IDL }) => {
-  const List = IDL.Rec();
-  const Embeddings = IDL.Vec(IDL.Float64);
+  const Message = IDL.Record({ 'content' : IDL.Text, 'sender' : IDL.Text });
   const ApiError = IDL.Variant({
     'ZeroAddress' : IDL.Null,
     'InvalidTokenId' : IDL.Null,
     'Unauthorized' : IDL.Null,
     'Other' : IDL.Text,
   });
-  const MemoryVectorsStoredResult = IDL.Variant({
-    'Ok' : IDL.Bool,
-    'Err' : ApiError,
-  });
-  const AuthRecord = IDL.Record({ 'auth' : IDL.Text });
-  const AuthRecordResult = IDL.Variant({ 'Ok' : AuthRecord, 'Err' : ApiError });
-  const MemoryVectorsCheckResult = IDL.Variant({
-    'Ok' : IDL.Bool,
-    'Err' : ApiError,
-  });
-  const CanisterType = IDL.Variant({
-    'Knowledgebase' : IDL.Null,
-    'Backend' : IDL.Null,
-  });
-  const CanisterCreationConfigurationInput = IDL.Record({
-    'canisterType' : CanisterType,
-  });
-  const CanisterCreationRecord = IDL.Record({
-    'creationResult' : IDL.Text,
-    'newCanisterId' : IDL.Text,
-  });
-  const CanisterCreationResult = IDL.Variant({
-    'Ok' : CanisterCreationRecord,
-    'Err' : ApiError,
-  });
-  const Message = IDL.Record({ 'content' : IDL.Text, 'sender' : IDL.Text });
   const ChatCreationResult = IDL.Variant({ 'Ok' : IDL.Text, 'Err' : ApiError });
   const Chat = IDL.Record({
     'id' : IDL.Text,
@@ -43,19 +16,6 @@ export const idlFactory = ({ IDL }) => {
     'firstMessagePreview' : IDL.Text,
   });
   const ChatResult = IDL.Variant({ 'Ok' : Chat, 'Err' : ApiError });
-  const AvailableCanistersRecord = IDL.Record({
-    'canisterType' : CanisterType,
-  });
-  const CanisterInfo = IDL.Record({
-    'canisterType' : CanisterType,
-    'creationTimestamp' : IDL.Nat64,
-    'canisterAddress' : IDL.Text,
-  });
-  const UserCanisterEntry = IDL.Record({ 'userCanister' : CanisterInfo });
-  const UserCanistersEntryResult = IDL.Variant({
-    'Ok' : UserCanisterEntry,
-    'Err' : ApiError,
-  });
   const ChatPreview = IDL.Record({
     'id' : IDL.Text,
     'creationTime' : IDL.Nat64,
@@ -67,16 +27,6 @@ export const idlFactory = ({ IDL }) => {
     'Err' : ApiError,
   });
   const ChatsResult = IDL.Variant({ 'Ok' : IDL.Vec(Chat), 'Err' : ApiError });
-  const MemoryVectorMetadata = IDL.Record({ 'id' : IDL.Int });
-  const MemoryVector = IDL.Record({
-    'content' : IDL.Text,
-    'metadata' : MemoryVectorMetadata,
-    'embedding' : Embeddings,
-  });
-  const MemoryVectorsResult = IDL.Variant({
-    'Ok' : IDL.Vec(MemoryVector),
-    'Err' : ApiError,
-  });
   const UserSettings = IDL.Record({
     'responseLength' : IDL.Text,
     'temperature' : IDL.Float64,
@@ -93,11 +43,6 @@ export const idlFactory = ({ IDL }) => {
     'emailAddress' : IDL.Text,
     'pageSubmittedFrom' : IDL.Text,
   });
-  List.fill(IDL.Opt(IDL.Tuple(Chat, List)));
-  const SearchKnowledgeBaseResult = IDL.Variant({
-    'Ok' : IDL.Text,
-    'Err' : ApiError,
-  });
   const SignUpFormInput = IDL.Record({
     'emailAddress' : IDL.Text,
     'pageSubmittedFrom' : IDL.Text,
@@ -112,37 +57,11 @@ export const idlFactory = ({ IDL }) => {
     'chatTitle' : IDL.Text,
   });
   const DeVinciBackend = IDL.Service({
-    'add_to_user_knowledgebase' : IDL.Func(
-        [IDL.Text, Embeddings],
-        [MemoryVectorsStoredResult],
-        [],
-      ),
-    'amiController' : IDL.Func([], [AuthRecordResult], ['query']),
-    'check_caller_has_memory_vectors_entry' : IDL.Func(
-        [],
-        [MemoryVectorsCheckResult],
-        ['query'],
-      ),
-    'createNewCanister' : IDL.Func(
-        [CanisterCreationConfigurationInput],
-        [CanisterCreationResult],
-        [],
-      ),
     'create_chat' : IDL.Func([IDL.Vec(Message)], [ChatCreationResult], []),
     'delete_chat' : IDL.Func([IDL.Text], [ChatResult], []),
     'delete_email_subscriber' : IDL.Func([IDL.Text], [IDL.Bool], []),
-    'getUserCanistersEntry' : IDL.Func(
-        [AvailableCanistersRecord],
-        [UserCanistersEntryResult],
-        ['query'],
-      ),
     'get_caller_chat_history' : IDL.Func([], [ChatsPreviewResult], ['query']),
     'get_caller_chats' : IDL.Func([], [ChatsResult], ['query']),
-    'get_caller_memory_vectors' : IDL.Func(
-        [],
-        [MemoryVectorsResult],
-        ['query'],
-      ),
     'get_caller_settings' : IDL.Func([], [UserSettingsResult], ['query']),
     'get_chat' : IDL.Func([IDL.Text], [ChatResult], ['query']),
     'get_email_subscribers' : IDL.Func(
@@ -151,25 +70,7 @@ export const idlFactory = ({ IDL }) => {
         ['query'],
       ),
     'greet' : IDL.Func([IDL.Text], [IDL.Text], []),
-    'isControllerLogicOk' : IDL.Func([], [AuthRecordResult], []),
-    'migrate_user_chats' : IDL.Func([IDL.Principal, List], [IDL.Bool], []),
-    'search_user_knowledgebase' : IDL.Func(
-        [Embeddings],
-        [SearchKnowledgeBaseResult],
-        [],
-      ),
-    'setCanisterCreationCanisterId' : IDL.Func(
-        [IDL.Text],
-        [AuthRecordResult],
-        [],
-      ),
-    'store_user_chats_memory_vectors' : IDL.Func(
-        [IDL.Vec(MemoryVector)],
-        [MemoryVectorsStoredResult],
-        [],
-      ),
     'submit_signup_form' : IDL.Func([SignUpFormInput], [IDL.Text], []),
-    'updateCanisterIsPrivate' : IDL.Func([IDL.Bool], [IDL.Bool], []),
     'update_caller_settings' : IDL.Func(
         [UserSettings],
         [UpdateUserSettingsResult],
@@ -181,7 +82,6 @@ export const idlFactory = ({ IDL }) => {
         [],
       ),
     'update_chat_metadata' : IDL.Func([UpdateChatObject], [ChatIdResult], []),
-    'whoami' : IDL.Func([], [IDL.Principal], ['query']),
   });
   return DeVinciBackend;
 };
